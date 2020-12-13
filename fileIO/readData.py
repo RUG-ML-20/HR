ddc3e72ece79503eaffc871d69ea0d087eccb812
JanRob23 <jan.henner.roberg@gmail.com>
2020-12-13 14:00:54 +0100

    data can now be loaded in either matrix or vector format

--- fileIO/readData.py
+++ fileIO/readData.py
@@ -12,8 +12,8 @@ def shuffling(X_train, y_train):
     shuffled_rows = np.random.permutation(X_train.shape[0])
     return X_train[shuffled_rows], y_train[shuffled_rows]
 
-# give it a digit array and the corresponding label, makes plot and prints label
-def display_digit(pic, label):
+# returns the matrix representation of a vector and if plot = True plots the digit
+def vector_to_pic_display(pic, label, plot=False):
     picmatreverse = np.zeros((15, 16))
     # the filling is done column wise
     picmatreverse = -pic.reshape(15, 16, order='F')
@@ -23,11 +23,11 @@ def display_digit(pic, label):
     picmat = np.transpose(picmat)
     picmat = np.flip(picmat, 0)
     picmat = np.flip(picmat, 1)
-    print(picmat)
-    plt_label = "This is a: " + str(label)
-    fig = plt.pcolor(picmat, cmap='Greys')
-    plt.show()
-    print("Label = " + str(label))
+    if plot:
+        fig = plt.pcolor(picmat, cmap='Greys')
+        plt.show()
+        print("Label = " + str(label))
+    return picmat
 
 def plotNumbers(data):
     fig, ax = plt.subplots(10, 10, sharex='col', sharey='row')
@@ -47,12 +47,16 @@ def plotNumbers(data):
             
     plt.show()
 
-def load(plot=True):
+'''
+load the data, Arguments are plot -> set to True if you want a plot of some example digits
+and vector_representation -> when false this function returns the data in matrix format, otherwise 
+data is returned in vectors
+'''
+def load(plot=False, vector_representation = False):
     file = "data/mfeat-pix.txt"
     mfeat_pix = np.loadtxt(file, dtype='i', delimiter=',')
     if plot:
         plotNumbers(mfeat_pix)
-
     # store labels for corresponding digits and shuffle
     data = mfeat_pix
     labels = []
@@ -65,10 +69,30 @@ def load(plot=True):
     data_X, data_Y = shuffling(data, labels_np)
     # split into training testing, atm 80% for training 20% for testing
     train_test_split = int(.8 * data.shape[0])
-    train_X = data_X[:train_test_split]
-    train_Y = data_Y[:train_test_split]
-    #display_digit(train_X[12], train_Y[12])
-    test_X = data_X[train_test_split:]
-    test_Y = data_Y[train_test_split:]
+    # data is returned in matrix format
+    if not vector_representation:
+        # convert the vectors in matrices and store
+        pic_data_x = {}
+        for i in range(0, len(data_X)):
+            pic_data_x[i] = vector_to_pic_display(data_X[i], data_Y[i], False)
+
+        # store training and testing matrices with indexes
+        train_X = []
+        test_X = []
+        for i in range(0, train_test_split):
+            train_X.append(pic_data_x[i])
+        for i in range(train_test_split, len(data_X)):
+            test_X.append(pic_data_x[i])
+        # convert to np arrays and store the corresponding labels
+        train_X = np.array(train_X)
+        train_Y = np.array(data_Y[:train_test_split])
+        test_X = np.array(test_X)
+        test_Y = np.array(data_Y[train_test_split:])
+    # data is returned in vector format
+    else:
+        train_X = np.array(data_X[:train_test_split])
+        test_X = np.array(data_X[train_test_split:])
+        train_Y = np.array(data_Y[:train_test_split])
+        test_Y = np.array(data_Y[train_test_split:])
 
     return train_X, train_Y, test_X, test_Y
--- launch.py
+++ launch.py
@@ -1,7 +1,7 @@
 import numpy
 from fileIO.readData import *
 
-train_x, train_y, test_x, test_y = load()
-linear_regression(train_x, train_y, test_x, test_y)
+train_x, train_y, test_x, test_y = load(plot=False, vector_representation=False)
+#linear_regression(train_x, train_y, test_x, test_y)
 
 #push test corine
