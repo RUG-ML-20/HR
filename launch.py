6c85b997ad8dde9f06287281b69ad90efc59ffb4
CharlieAprog <charlie.albietz.uni@gmail.com>
2020-12-14 19:51:03 +0100

    removing pushtests

--- launch.py
+++ launch.py
@@ -2,12 +2,9 @@ import numpy
 from fileIO.readData import *
 from Components.pca import pca
 
-train_x, train_y, test_x, test_y = load(load(plot=False, vector_representation=False)
+train_x, train_y, test_x, test_y = load(plot=False, vector_representation=False)
 pcaTrain, pcaTest, percentageExplainedPC  = pca(train_x, test_x, nComponents=60)
 
 linear_regression(train_x, train_y, test_x, test_y)
 
 linear_regression(pcaTrain, train_y, pcaTest, test_y)
-
-#push test niclas
-#push test jan
