24400a4cea6d206ad5894cc37c1c30e7e4d3da3d
CorineNijhof <c.nijhof@student.rug.nl>
2021-01-21 11:44:55 +0100

    fixed float range using np.arange + cleaned up to Python standards

--- launch.py
+++ launch.py
@@ -3,22 +3,12 @@ from FileIO import load
 from Components import *
 from Visualisation import plotNumbers, plotTrainTestPerformance
 
+x_train, y_train, x_test, y_test = load(.5, plot=True)
 
-x_train, y_train, x_test, y_test = load(.5)
+train, test, m, change = crossvalidationCNN(x_train, y_train, 10)
+plotTrainTestPerformance(train, test, change, x_values=m)
 
-# x = [[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4],[5,5,5,5],[6,6,6,6],[7,7,7,7],[8,8,8,8],[9,9,9,9],[10,10,10,10],[11,11,11,11],[12,12,12,12],[13,13,13,13],[14,14,14,14],[15,15,15,15],[16,16,16,16]]
-# y = [111,222,333,444,555,666,777,888,999,1000,1111,1222,1333,1444,1555,1666]
-# x = np.array(x)
-# y = np.array(y)
-# crossvalidationCNN(x, y, 4)
-#plotNumbers(x_train)
-train, test, ran, change = crossvalidationCNN(x_train, y_train, 10)
-train = get_averages(train)
-test = get_averages(test)
-plotTrainTestPerformance(train, test, change, )
-
-
-#data_analysis(x_train, y_train, x_test, y_test)
+# data_analysis(x_train, y_train, x_test, y_test)
 
 # print(linear_regression(x_train,y_train, x_test, y_test))
 
